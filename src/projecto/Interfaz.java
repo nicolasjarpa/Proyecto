b1b2c7f92f4bbce6f597515d379b73ff1e1a7e00
BuildTools <pegajardo2021@udec.cl>
2022-11-24 21:49:23 -0300

    Beta 1.01: We added car movement in a very basic version as a rectangle

--- src/projecto/Interfaz.java
+++ src/projecto/Interfaz.java
@@ -1,4 +1,3 @@
-
 package projecto;
 
 import java.awt.Polygon;
@@ -13,104 +12,168 @@ import java.util.Set;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import java.awt.event.*;
 
-
-public class Interfaz extends JFrame implements ActionListener{
+public class Interfaz extends JPanel implements ActionListener, KeyListener {
 
     public JPanel juego = new JPanel();
-    private auto a = new auto();
+    private auto auto = new auto();
     public String direction = null;
-    int x=50;
-    int y=140;
+    float x = 20f;   // posicion x auto
+    float y = 20f;   // posicion y auto
+    float angle = 0f;
     private Colisiones coli;
-    public void actionPerformed(ActionEvent e){
-        
+    private boolean w, a, s, d, up, down, right, left;
+    Polygon p = new Polygon();
+
+    public void actionPerformed(ActionEvent e) {
+
     }
-    
-      public Interfaz(){
+
+    public Interfaz() {
 
         this.setSize(960, 780);
-        this.setTitle(" Super Auto");
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.getContentPane().add(juego);
-        
-        KeyListener listener = new KeyListener() {
-			public void keyTyped(KeyEvent e) {
-			}
-
-			
-			public void keyPressed(KeyEvent e) {
-				
-                                if(KeyEvent.getKeyText(e.getKeyCode())=="Right"){
-                                    x=x+10;
-                                    direction = "Right";
-                                    Colisiones();
-                                    repaint();
-                                }
-                                if(KeyEvent.getKeyText(e.getKeyCode())=="Left"){
-                                    x=x-10;
-                                    direction = "Left";
-                                    Colisiones();
-                                    repaint();
-                                }
-                                if(KeyEvent.getKeyText(e.getKeyCode())=="Up"){
-                                    y=y-10;
-                                    direction = "Up";
-                                    Colisiones();
-                                    repaint();
-                                }
-                                if(KeyEvent.getKeyText(e.getKeyCode())=="Down"){
-                                    y=y+10;
-                                    direction = "Down";
-                                    Colisiones();
-                                    repaint();
-                                }
-                                
-			}
-
-			
-			public void keyReleased(KeyEvent e) {
-				
-                                direction = null;
-			}
-        };
-		addKeyListener(listener);
-		setFocusable(true);
 
     }
-      public int Colisiones(){
-          if(x<=10){
-              x=x+10;
-              repaint();
-          }
-          if(x>=950){
-          x=x-10;
-          }
-          if(y<=45){
-              y=y+10;
-          }
-          if(y>=755){
-              y=y-10;
-          }
-          return 0;
-      }
-      
-    
-    public void paint(Graphics g){
-        super.paint(g);
-        a.paint(g);
 
-        Polygon p   = new Polygon();
-        p.addPoint(x+10, y+20);
-        p.addPoint(x+10, y+ -20);
-        p.addPoint(x-10, y-20);
-        p.addPoint(x-10, y+20);
+    public int Colisiones() {
+        if (x <= 10) {
+            x = x + 10;
+
+        }
+        if (x >= 950) {
+            x = x - 10;
+        }
+        if (y <= 45) {
+            y = y + 10;
+        }
+        if (y >= 755) {
+            y = y - 10;
+        }
+        return 0;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        if (w) {
+            x += 0.1f * Math.cos(Math.toRadians(angle));
+            y += 0.1f * Math.sin(Math.toRadians(angle));
+        }
+        if (s) {
+            x -= 0.1f * Math.cos(Math.toRadians(angle));
+            y -= 0.1f * Math.sin(Math.toRadians(angle));
+        }
+        if (a) {
+            angle -= 0.05f;
+        }
+        if (d) {
+            angle += 0.05f;
+        }
+        
+        update_auto();
+        /*p = new Polygon();
+        p.addPoint((int)x+5, (int)y+10);
+        p.addPoint((int)x+5, (int)y-10);
+        p.addPoint((int)x-5, (int)y-10);
+        p.addPoint((int)x-5, (int)y+10); 
+        System.out.println(p.npoints); */
         g.setColor(Color.red);
         g.fillPolygon(p);
+        Toolkit.getDefaultToolkit().sync();
         g.dispose();
         repaint();
-        
+
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            w = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            a = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            s = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D) {
+            d = true;
+        }
+        /*if (e.getKeyCode() == KeyEvent.VK_UP) {
+            up = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            left = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            down = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            right = true;
+        }*/
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            w = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            a = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            s = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D) {
+            d = false;
+        }
+
+        /*if (e.getKeyCode() == KeyEvent.VK_UP) {
+            up = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            left = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            down = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            right = false;
+        }*/
+    }
+
+    public void update_auto() {
+        float cos = (float) Math.cos(Math.toRadians(angle));
+        float sin = (float) Math.sin(Math.toRadians(angle));
+
+        p = new Polygon();
+
+
+        float px = 10f;
+        float py = -20f;
+        p.addPoint((int) (x + px * cos - py * sin), (int) (y + px * sin + py * cos));
+
+
+        px = -20f;
+        py = -5f;
+        p.addPoint((int) (x + px * cos - py * sin), (int) (y + px * sin + py * cos));
+
+
+        px = -10f;
+        py = 20f;
+        p.addPoint((int) (x + px * cos - py * sin), (int) (y + px * sin + py * cos));
+
+
+        px = 20f;
+        py = 5f;
+        p.addPoint((int) (x + px * cos - py * sin), (int) (y + px * sin + py * cos));
+    }
+
 }
--- src/projecto/Projecto.java
+++ src/projecto/Projecto.java
@@ -1,12 +1,25 @@
 
 package projecto;
 
+import javax.swing.JFrame;
+
 
 public class Projecto {
 
     public static void main(String[] args) {
-        Interfaz ventana = new Interfaz();
+        JFrame ventana = new JFrame();
+        ventana.setSize(960, 780);
+        ventana.setTitle(" Super Auto");
+        ventana.setLocationRelativeTo(null);
+        ventana.setResizable(false);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
         ventana.setVisible(true);
+        
+        Interfaz panel = new Interfaz();
+        ventana.add(panel);
+        ventana.addKeyListener(panel);
     }
     
 }
